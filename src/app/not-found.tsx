2415f65ad981b08124a9a4d86a8463ef3b31fe0d
Caio Mognatti <caiocsm97@gmail.com>
2024-07-13 15:09:44 -0300

    fix: makes the not-found page responsive

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,18 +4,26 @@ import wppIcon from "../assets/icons/whatsappIcon.svg";
 import leftArrowIcon from "../assets/icons/leftArrowIcon.svg";
 import Button from "@/GlobalComponents/Button";
 import { routes } from "./lib/routes";
+import { externalLinks } from "./lib/externalLinks";
 
 export default function Custom404() {
   return (
     <div className="mt-[10rem] mb-[4rem] flex flex-col w-full relative">
-      <div className="mt-[8em] flex flex-col justify-center items-center w-full mb-r4">
-        <h2 className="text-dlxl text-primary-900 font-Mulish font-bold">Estamos em construção</h2>
-        <p className="text-h1 text-neutral-700">Volte em breve</p>
+      <div className="mt-[8em] sm:mt-r22 flex flex-col justify-center items-center w-full mb-r4">
+        <h2 className="text-dlxl sm:text-h4 text-primary-900 font-Mulish font-bold mb-[12px] sm:mb-r2 ">
+          Estamos em construção
+        </h2>
+        <p className="text-h1 sm:text-pl text-neutral-700 sm:mb-r4">Volte em breve</p>
       </div>
       <div className="w-full flex flex-col items-center gap-6">
-        <Button color="blue-light" rounded bold className="w-72 py-3 flex justify-center items-center relative">
-          <Image src={wppIcon} alt="whastapp logo" className="absolute left-[14%] top-[30%]" />
-          Entrar em contato
+        <Button
+          color="blue-light"
+          rounded
+          link={externalLinks.whatsappURL}
+          className="w-72 py-3 flex justify-center items-center relative"
+        >
+          <Image src={wppIcon} alt="whastapp logo" className="absolute left-[15%] top-[30%]" />
+          <span className="ml-[10px]">Entrar em contato</span>
         </Button>
         <Button
           color="blue-outline"
@@ -24,7 +32,7 @@ export default function Custom404() {
           className="w-72 flex justify-center items-center relative"
           link={routes.home}
         >
-          <Image src={leftArrowIcon} alt="whastapp logo" className="absolute left-[20%] top-[20%]" />
+          <Image src={leftArrowIcon} alt="whastapp logo" className="absolute left-[15%] top-[22%]" />
           Página Inicial
         </Button>
       </div>
