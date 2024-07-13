b8d301b1576239a8e4b502e51578ae89496d9a91
Caio Mognatti <caiocsm97@gmail.com>
2024-07-13 15:15:07 -0300

    fix: removes a visual bug on mobile menu when the container component is called on the page

--- src/GlobalComponents/Container/index.tsx
+++ src/GlobalComponents/Container/index.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 
 export default function Container({ children, id }: ContainerProps) {
   return (
-    <div className={`parent pb-10 sm:pb-12  flex flex-col w-svw relative scroll-mt-28 z-50`} id={id}>
+    <div className={`parent pb-10 sm:pb-12  flex flex-col w-svw relative scroll-mt-28 z-20`} id={id}>
       <div className={`children mx-r11 sm:mx-r4 flex flex-col`}>{children}</div>
     </div>
   );
