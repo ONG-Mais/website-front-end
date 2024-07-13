f62b3acc85c9219404b25592d0eb949a646e0f7b
Caio Mognatti <caiocsm97@gmail.com>
2024-07-13 15:16:04 -0300

    feat: makes a dropdown menu on the header for mobile and desktop to deal with sublinks

--- src/GlobalComponents/Header/Nav/HamburguerMenu/index.tsx
+++ src/GlobalComponents/Header/Nav/HamburguerMenu/index.tsx
@@ -1,35 +1,86 @@
+"use client";
+
 import Image from "next/image";
 import closeIcon from "../../closeHamburguerIcon.svg";
 import { link } from "../../types";
 import Link from "next/link";
 import Button from "@/GlobalComponents/Button";
 import { routes } from "@/app/lib/routes";
+import downArrow from "../../../../assets/icons/selectArrow.svg";
+import { useState } from "react";
 
 type HamburguerProps = {
-  showMenu: boolean;
+  showMobileMenu: boolean;
   links: link[];
   handleMenu(): void;
 };
 
-export default function HamburguerMenu({ showMenu, links, handleMenu }: HamburguerProps) {
+export default function HamburguerMenu({ showMobileMenu, links, handleMenu }: HamburguerProps) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   return (
     <div
-      className={`hamburguer-menu hidden z-40 ${showMenu ? "sm:flex" : "sm:hidden"}`}
+      className={`hamburguer-menu hidden z-40 ${showMobileMenu ? "sm:flex" : "sm:hidden"}`}
       data-testid="hamburguer-container"
     >
       <div className="modal-bg hidden sm:block bg-neutral-800 opacity-30 fixed top-0 left-0 m-none w-svw h-svh z-0 overflow-hidden" />
-      <div className="mobile-menu hidden sm:bg-neutral-0 sm:absolute sm:top-0 sm:right-0 sm:h-[93vh] sm:w-svw sm:flex rounded-b-3xl">
+      <div
+        className={`mobile-menu hidden sm:bg-neutral-0 sm:absolute sm:top-0 sm:right-0 sm:h-fit sm:pb-8 sm:w-svw sm:flex rounded-b-3xl z-30`}
+      >
         <ul className="text-neutral-900 flex flex-col w-svw justify-start sm:mt-[20vh] sm:ml-px5">
-          {links.map((link) => (
-            <Link
-              key={link.id}
-              href={link.to}
-              onClick={() => handleMenu()}
-              className="text-pl font-semibold border-b border-b-neutral-200 py-5"
-            >
-              {link.title}
-            </Link>
-          ))}
+          {links.map((link) => {
+            if (link.sublinks) {
+              return (
+                <div
+                  key={link.id}
+                  className={`flex flex-col ${showDropdown ? "pb-2" : "border-b border-b-neutral-200 py-5"} `}
+                >
+                  <div
+                    role="button"
+                    tabIndex={0}
+                    id="dorpdown-title"
+                    onClick={() => setShowDropdown((prev) => !prev)}
+                    className="flex w-full justify-between"
+                  >
+                    <p
+                      className={`text-pl font-semibold w-full ${showDropdown && "border-b border-b-neutral-200 py-5"}`}
+                    >
+                      {link.title}
+                    </p>
+                    <Image
+                      src={downArrow}
+                      alt="↓"
+                      className={`${showDropdown && "border-b border-b-neutral-200 py-5 rotate-180"}`}
+                    />
+                  </div>
+                  <div id="dropdown-menu" className={`${showDropdown ? "flex flex-col" : "hidden"}`}>
+                    {link.sublinks.map((sublink) => (
+                      <Link
+                        key={sublink.id}
+                        href={sublink.to}
+                        onClick={() => handleMenu()}
+                        className="text-pm border-b border-b-neutral-200 py-3"
+                      >
+                        {sublink.title}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              );
+            }
+            if (link.to) {
+              return (
+                <Link
+                  key={link.id}
+                  href={link.to}
+                  onClick={() => handleMenu()}
+                  className="text-pl font-semibold border-b border-b-neutral-200 py-5"
+                >
+                  {link.title}
+                </Link>
+              );
+            }
+          })}
           <div className="w-full flex justify-center items-center">
             <Button color="green-light" className="mt-r8" rounded link={routes.donate} size="lg">
               Doe agora
--- src/GlobalComponents/Header/Nav/index.tsx
+++ src/GlobalComponents/Header/Nav/index.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import hamburguerIcon from "../hamburguerIcon.svg";
 import { handleScroll } from "@/app/lib/handleScroll";
 import { link } from "../types";
+import downArrow from "../../../assets/icons/selectArrow.svg";
 
 export const links: link[] = [
   {
@@ -24,7 +25,23 @@ export const links: link[] = [
   {
     id: 3,
     title: "Como ajudar",
-    to: "/como-ajudar",
+    sublinks: [
+      {
+        id: 1,
+        title: "Doação",
+        to: routes.donate,
+      },
+      {
+        id: 2,
+        title: "Voluntariado",
+        to: routes.volunteer,
+      },
+      {
+        id: 3,
+        title: "Empresa Parceira",
+        to: routes.partnerUp,
+      },
+    ],
   },
   {
     id: 4,
@@ -39,10 +56,11 @@ export const links: link[] = [
 ];
 
 export default function Nav() {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   function handleMenu() {
-    setShowMenu((prev) => !prev);
+    setShowMobileMenu((prev) => !prev);
     handleScroll();
   }
 
@@ -51,16 +69,55 @@ export default function Nav() {
       <Image
         src={hamburguerIcon}
         alt="menu"
-        className={`sm:block hidden sm:z-50 ${showMenu ? "hidden" : "block"}`}
+        className={`sm:block hidden sm:z-50 ${showMobileMenu ? "hidden" : "block"}`}
         onClick={() => handleMenu()}
         data-testid="hamburguer-icon"
       />
-      {showMenu && <HamburguerMenu {...{ showMenu, handleMenu, links }} />}
-      {links.map((link) => (
-        <Link key={link.id} href={link.to} className="text-pl sm:hidden" style={{ wordSpacing: "0.25em" }}>
-          {link.title}
-        </Link>
-      ))}
+      {showMobileMenu && <HamburguerMenu {...{ showMobileMenu, handleMenu, links }} />}
+      {links.map((link) => {
+        if (link.sublinks) {
+          return (
+            <div
+              key={link.id}
+              className={`flex-col text-pl sm:hidden h-[25px] w-fit items-center relative`}
+              onClick={() => setShowDropdown((prev) => !prev)}
+            >
+              <div className="flex gap-3 items-center ">
+                {link.title}
+                <Image
+                  src={downArrow}
+                  alt="↓"
+                  className={`${showDropdown && "rotate-180"} duration-300 saturate-200`}
+                />
+              </div>
+              <div
+                className={`${
+                  showDropdown
+                    ? " bg-neutral-0 text-neutral-900 text-ps gap-y-3 p-6 visible w-[180px] top-10 -left-7 transition-spacing duration-200 shadow-lg drop-shadow-lg"
+                    : "invisible w-0"
+                } flex flex-col text-neutral-900 `}
+              >
+                {link.sublinks.map((sublink) => (
+                  <Link
+                    href={sublink.to}
+                    key={sublink.id}
+                    className="py-2 px-1 hover:bg-primary-300 transition-bgAndSpacing duration-200 "
+                  >
+                    {sublink.title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          );
+        }
+        if (link.to) {
+          return (
+            <Link key={link.id} href={link.to} className="text-pl sm:hidden" style={{ wordSpacing: "0.25em" }}>
+              {link.title}
+            </Link>
+          );
+        }
+      })}
       <Button color="green-light" className="px-5 sm:hidden" rounded link={routes.donate}>
         Doe agora
       </Button>
