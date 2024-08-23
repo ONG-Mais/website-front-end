4b0467b3ceb51619d16c97c11c3de5dfbc80c97d
Caio Mognatti <caiocsm97@gmail.com>
2024-08-23 14:17:53 -0300

    fix: changes the name from noMobileMargin to noMobileXMargin

--- src/GlobalComponents/Container/index.tsx
+++ src/GlobalComponents/Container/index.tsx
@@ -3,13 +3,13 @@ import { ReactNode } from "react";
 interface ContainerProps {
   children?: ReactNode;
   id?: string;
-  noMobileMargin?: boolean;
+  noMobileXMargin?: boolean;
 }
 
-export default function Container({ children, id, noMobileMargin }: ContainerProps) {
+export default function Container({ children, id, noMobileXMargin }: ContainerProps) {
   return (
-    <div className={`parent pb-10 sm:pb-12 flex flex-col w-svw relative scroll-mt-28 z-20`} id={id}>
-      <div className={`children ${noMobileMargin ? "sm:mx-none" : "sm:mx-r4"} mx-r11 flex flex-col`}>{children}</div>
+    <div className={`parent pb-10 sm:pb-8 flex flex-col w-svw relative scroll-mt-28 z-20`} id={id}>
+      <div className={`children ${noMobileXMargin ? "sm:mx-none" : "sm:mx-r4"} mx-r11 flex flex-col`}>{children}</div>
     </div>
   );
 }
