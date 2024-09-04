f0df4c3d74821a3a3211a2bfbbb4f5286d3972a0
Caio Mognatti <caiocsm97@gmail.com>
2024-09-04 17:01:47 -0300

    feat: creates a new hook to handle the user window scroll on the page

---
+++ src/app/lib/hooks/useWindowYpos.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function useWindowYpos() {
+  const yPos = window.scrollY;
+  const [currentY, setCurrentY] = useState(yPos);
+
+  function handleScroll() {
+    setCurrentY(window.scrollY);
+  }
+
+  useEffect(() => {
+    document.addEventListener("scroll", handleScroll);
+    return () => document.removeEventListener("scroll", handleScroll);
+  }, [window.scrollY]);
+
+  return { currentY };
+}
