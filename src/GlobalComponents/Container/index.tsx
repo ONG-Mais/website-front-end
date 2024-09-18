e1529263074042fb812c31af57f84eb872742da6
Caio Mognatti <caiocsm97@gmail.com>
2024-09-18 09:34:17 -0300

    fix: creates a new prop in the container component allowing it to change the margin if this is the first thing on a new page

--- src/GlobalComponents/Container/index.tsx
+++ src/GlobalComponents/Container/index.tsx
@@ -4,11 +4,17 @@ interface ContainerProps {
   children?: ReactNode;
   id?: string;
   noMobileXMargin?: boolean;
+  isHero?: boolean;
 }
 
-export default function Container({ children, id, noMobileXMargin }: ContainerProps) {
+export default function Container({ children, id, noMobileXMargin, isHero }: ContainerProps) {
   return (
-    <div className={`parent pb-10 sm:pb-8 flex flex-col w-svw relative scroll-mt-28 z-20`} id={id}>
+    <div
+      className={`parent pb-10 ${
+        isHero && "pt-28 sm:text-center sm:pt-16"
+      } sm:pb-8 flex flex-col w-svw relative scroll-mt-28 z-20`}
+      id={id}
+    >
       <div className={`children ${noMobileXMargin ? "sm:mx-none" : "sm:mx-r4"} mx-r11 flex flex-col`}>{children}</div>
     </div>
   );
