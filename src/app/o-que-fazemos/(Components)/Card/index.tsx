db4aa75742f1620c08d7f21c6718c7ada009d42a
Caio Mognatti <caiocsm97@gmail.com>
2024-11-08 14:28:12 -0300

    fix: improves responsiveness on o-que-fazemos page and the cards inside of it

--- src/app/o-que-fazemos/(Components)/Card/index.tsx
+++ src/app/o-que-fazemos/(Components)/Card/index.tsx
@@ -7,9 +7,9 @@ type Card = {
 
 export default function Card({ card }: Card) {
   return (
-    <div className="w-[335px] flex flex-col rounded-xl shadow-lg xs:w-full">
-      <div className="w-[335px] h-[235px] xs:w-full ">
-        <div style={{ backgroundImage: `url(${card.img})` }} className="w-full h-full bg-cover" />
+    <div className="w-[30%] min-w-[315px] flex flex-col rounded-xl shadow-lg ">
+      <div className="w-[100%] h-[235px] rounded-xl  ">
+        <div style={{ backgroundImage: `url(${card.img})` }} className="w-full h-full bg-cover rounded-t-xl" />
       </div>
       <div className="w-full text-center text-h5 font-Mulish font-bold px-7 pb-2 pt-5">{card.title}</div>
       <div className="w-full text-pm px-7 pb-5 text-start">{card.desc}</div>
