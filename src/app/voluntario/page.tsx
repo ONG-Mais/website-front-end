c0d7ce4e7e425a57b33345a4451765b3989891d7
Caio Mognatti <caiocsm97@gmail.com>
2025-02-21 18:12:07 -0300

    feat: adjust the volunteer page to use the new hook

--- src/app/voluntario/page.tsx
+++ src/app/voluntario/page.tsx
@@ -1,95 +1,43 @@
 "use client";
 
-import { useState } from "react";
-import FormBanner from "@/GlobalComponents/FormBanner";
+import FormFeedbackMessage from "@/GlobalComponents/Form/FormFeedbackMessage";
 import bannerImg from "../../../public/images/thumbs_up_batman_kid.jpg";
-import "./styles.css";
-import Form from "@/GlobalComponents/Form";
-import { FormData, FormSelect } from "@/GlobalComponents/Form/formTypes";
-import { Volunteer } from "./types";
 import useCreateVolunteer from "../lib/hooks/useCreateVolunteer";
-import FormSucessMessage from "@/GlobalComponents/Form/FormSucessMessage";
+import FormBanner from "@/GlobalComponents/FormBanner";
+import Form from "@/GlobalComponents/Form";
+import Loader from "@/assets/icons/loader";
 
 export default function Page() {
-  let volunteer: Volunteer;
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [phone, setPhone] = useState<string>("");
-  const [city, setCity] = useState<string>("");
-  const [state, setState] = useState<string>("");
   const [{ isError, isLoading, isSucess, postVolunteer }] = useCreateVolunteer();
 
-  const formInputs: FormData[] = [
-    {
-      id: 1,
-      name: "name",
-      placeholder: "Digite seu nome completo",
-      label: "Nome",
-      type: "text",
-      value: name,
-      setter: setName,
-    },
-    {
-      id: 2,
-      name: "email",
-      placeholder: "Digite seu melhor email",
-      label: "Email",
-      type: "email",
-      value: email,
-      setter: setEmail,
-    },
-    {
-      id: 3,
-      name: "phone",
-      placeholder: "(00) 00000 0000",
-      label: "Celular/Whatsapp",
-      type: "text",
-      value: phone,
-      setter: setPhone,
-    },
-  ];
-  const formSelect: FormSelect[] = [
-    {
-      id: 1,
-      label: "Estado",
-      name: "estado",
-      placeholder: "Escolha seu estado",
-      value: state,
-      setter: setState,
-      options: [
-        {
-          id: 1,
-          title: "Distrito Federal",
-        },
-      ],
-    },
-    {
-      id: 2,
-      label: "Cidade",
-      name: "cidade",
-      placeholder: "Escolha sua cidade",
-      value: city,
-      setter: setCity,
-      options: [
-        {
-          id: 1,
-          title: "Brasília",
-        },
-      ],
-    },
-  ];
+  if (isError) {
+    return (
+      <FormBanner image={bannerImg.src} imagePosition="center">
+        <FormFeedbackMessage variant="error" />
+      </FormBanner>
+    );
+  }
 
   if (isSucess) {
     return (
       <FormBanner image={bannerImg.src} imagePosition="center">
-        <FormSucessMessage variant="volunteer" />
+        <FormFeedbackMessage variant="volunteer" />
       </FormBanner>
     );
   }
 
   return (
     <FormBanner image={bannerImg.src} imagePosition="right">
-      <Form formAction={postVolunteer} title="Seja um voluntário" {...{ formInputs, formSelect }} />
+      {isLoading ? (
+        <div className="absolute top-1/3 flex align-center justify-start gap-8 sm:flex-col sm:relative sm:gap-4">
+          <p className="text-pl text-neutral-100  xs:text-pm sm:text-center -ml-r6">
+            Estamos enviando os dados, aguarde um momento
+          </p>
+          <Loader fill="#fff" stroke="#fff" show={isLoading} size="10" />
+        </div>
+      ) : (
+        <Form formAction={postVolunteer} title="Seja um voluntário" />
+      )}
     </FormBanner>
   );
 }
