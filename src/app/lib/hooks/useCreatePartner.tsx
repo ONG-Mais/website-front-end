52532744857d46864117c6813e6e17c1bc31f871
Caio Mognatti <caiocsm97@gmail.com>
2025-02-21 18:13:56 -0300

    feat: creates the hook to handle create company https requests

--- src/app/lib/hooks/useCreatePartner.tsx
+++ src/app/lib/hooks/useCreatePartner.tsx
@@ -15,11 +15,29 @@ export default function useCreatePartner() {
   const [isSucess, setIsSucess] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const endpoint = "company";
 
   async function postPartner(partner: Partner) {
     setIsLoading(true);
+    const body = {
+      name: partner.name,
+      email: partner.email,
+      telefone: partner.phone,
+      state: partner.location.state.nome,
+      city: partner.location.city.nome,
+      type: "company",
+    };
     try {
-      await console.log(partner); //TBD
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${endpoint}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
       setIsLoading(false);
       setIsSucess(true);
     } catch (error) {
