ee11d09fe4e8f43c02aed7ebba37bbb30ecc70d2
Caio Mognatti <caiocsm97@gmail.com>
2025-02-21 18:17:31 -0300

    style: small style changes to handle the loader on the pages

--- src/app/o-que-fazemos/(Components)/Card/index.tsx
+++ src/app/o-que-fazemos/(Components)/Card/index.tsx
@@ -7,13 +7,13 @@ type Card = {
 
 export default function Card({ card }: Card) {
   return (
-    <div className="w-[30%] min-w-[315px] flex flex-col rounded-xl shadow-lg ">
+    <div className="relative w-[30%] min-w-[315px] h-[470px] flex flex-col rounded-xl shadow-lg ">
       <div className="w-[100%] h-[235px] rounded-xl  ">
         <div style={{ backgroundImage: `url(${card.img})` }} className="w-full h-full bg-cover rounded-t-xl" />
       </div>
       <div className="w-full text-center text-h5 font-Mulish font-bold px-7 pb-2 pt-5">{card.title}</div>
       <div className="w-full text-pm px-7 pb-5 text-start">{card.desc}</div>
-      <div className="px-7 pb-7">
+      <div className="absolute w-full bottom-0 px-7 pb-7">
         <Button color="green-outline" link={card.url} size="lg" bold className="font-Mulish">
           <span className="text-pl">Saiba mais</span>
         </Button>
